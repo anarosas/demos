8a5377f0e69e488f21758588d2ac360a92c65ea8
Ana Rosas <a.rosas10@gmail.com>
2018-09-12 16:38:25 -0500

    Use mocha for tests

---
+++ test/app_test.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const assert = require('assert')
+//const app = require('../app')
+
+describe('app', function() {
+
+
+  describe('#start', function() {
+    it('starts a process listening on port 8000', function(done) {
+
+      assert('👋🌍');
+    })
+  })
+})
